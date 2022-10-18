964ee38d44421d2739df5975975e9aa5c66d08f5
papasmf1 <papasmf1@gmail.com>
2022-10-18 16:29:42 +0900

    명시적인 코딩

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,16 @@
+#전역변수 
 str = "Not Class Member"
+#파이썬은 모호한 것보다는 명시적인 것이 좋다! 
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(self.str)
 
+#인스턴스 생성 
 g = GString()
 g.set("First Message")
 g.print()
